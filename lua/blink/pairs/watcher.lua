34e667657804518db914810ebe91836cff0885df
Stefan Boca <stefan.r.boca@gmail.com>
2025-06-19 13:39:01 -0700

    fix(highlights): catch errors in parsing
    Parsing on buffers with invalid unicode fails, so catch those errors and
    stop parsing.
    
    Closes #38

--- lua/blink/pairs/watcher.lua
+++ lua/blink/pairs/watcher.lua
@@ -16,7 +16,8 @@ local function parse_buffer(bufnr, start_line, old_end_line, new_end_line)
   local lines = vim.api.nvim_buf_get_lines(bufnr, start_line or 0, new_end_line or -1, false)
 
   local rust = require('blink.pairs.rust')
-  local did_parse = rust.parse_buffer(bufnr, vim.bo[bufnr].filetype, lines, start_line, old_end_line, new_end_line)
+  local ok, ret = pcall(rust.parse_buffer, bufnr, vim.bo[bufnr].filetype, lines, start_line, old_end_line, new_end_line)
+  local did_parse = ok and ret
 
   if did_parse and require('blink.pairs.config').debug then
     vim.print('parsing time: ' .. (vim.uv.hrtime() - start_time) / 1e6 .. ' ms')
