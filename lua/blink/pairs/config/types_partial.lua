05afa3611bf1fcd6afb62b11f1a58f5eb6b2d0c5
Stefan Boca <stefan.r.boca@gmail.com>
2025-08-02 12:12:35 -0700

    refactor(types): move non-partial type definitions out of `types_partial.lua`

--- lua/blink/pairs/config/types_partial.lua
+++ lua/blink/pairs/config/types_partial.lua
@@ -1,24 +1,7 @@
 --- @class (exact) blink.pairs.Config : blink.pairs.ConfigStrict, {}
 --- @field mappings? blink.pairs.MappingsConfigPartial
 --- @field highlights? blink.pairs.HighlightsConfigPartial
---- @field debug? boolean
 
-
---- @alias blink.pairs.RuleDefinitions table<string, string | blink.pairs.RuleDefinition | blink.pairs.RuleDefinition[]>
-
---- @class (exact) blink.pairs.RuleDefinition
---- @field [1] string Closing character (e.g. { ')' }) or opening character if two characters are provided (e.g. {'(', ')'})
---- @field [2]? string Closing character (e.g. {'(', ')'})
---- @field priority? number
---- @field cmdline? boolean
---- @field languages? string[]
---- @field when? fun(ctx: blink.pairs.Context): boolean
---- @field open? boolean | fun(ctx: blink.pairs.Context): boolean Whether to open the pair
---- @field close? boolean | fun(ctx: blink.pairs.Context): boolean Whether to close the pair
---- @field open_or_close? boolean | fun(ctx: blink.pairs.Context): boolean Whether to open or close the pair, used in-place of `open` and `close` when the open and close are the same (such as for '' or "")
---- @field enter? boolean | fun(ctx: blink.pairs.Context): boolean
---- @field backspace? boolean | fun(ctx: blink.pairs.Context): boolean
---- @field space? boolean | fun(ctx: blink.pairs.Context): boolean
 --- @class (exact) blink.pairs.MappingsConfigPartial : blink.pairs.MappingsConfig, {}
 
 --- @class (exact) blink.pairs.HighlightsConfigPartial : blink.pairs.HighlightsConfig, {}
